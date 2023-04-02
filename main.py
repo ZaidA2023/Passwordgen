3c0c049b846e22fd309e619c9ed4b7e5536e227d
Zaid Albustami <zaida2023@outlook.com>
2023-04-01 23:35:34 -0500

    Added copying and dynamic elements to UI

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import random
+import tkinter as tk
+from tkinter import *
+import pyperclip
+
+win = Tk()
+win.geometry("200x100")
+
 
 def ranspec():
     special = ['!','"','@','#','$','%','^','&','*','(',')','-','_','+','=','\\\\','|','{','[','}',']',';',':','<','>',',','.','?','/','`','~']
@@ -12,14 +19,41 @@ def ranalph():
     wah = alphabet[random.randint(0,25)]
     return wah
 
-password = ""
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-for i in range(20):
-    x = random.randint(0,2)
-    if x == 0:
-        password = password + ranspec()
-    elif x==1:
-        password = password + str(rannum())
-    elif x==2:
-        password = password + ranalph()
-print(password)
+def make():
+    password = " "
+    count = 0
+    count=count+1
+    for i in range(20):
+        x = random.randint(0,2)
+        if x == 0:
+            password = password + ranspec()
+        elif x==1:
+            password = password + str(rannum())
+        elif x==2:
+            password = password + ranalph()
+    display.configure(text=password)
+    display.pack()
+    return password
+    
+ 
+display =Label(win,text="")
+button_dict={}
+option= ["Generate", "Copy"]
+
+b2 = Button(text = "Copy")
+def save():
+    xa = make()
+    b2.configure(command=lambda : pyperclip.copy(xa))
+    b2.pack()
+b1 = Button(text = "Generate",  command=save)
+   #button_dict[i]=ttk.Button(win, text=i, command=make())
+   #button_dict[i].pack()
+b1.pack()
+
+
+win.mainloop()
+
+
+
+
+
